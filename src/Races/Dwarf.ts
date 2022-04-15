1801f0cc3abf8802b22abb5d197fccc4da9bd659
Arthur Simões <arthursimoes155@gmail.com>
2022-04-15 19:17:22 -0300

    Feat: Create Dwarf, Elf, Halfling and Orc class

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _dwarfInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this._dwarfInstances += 1;
+    return this._dwarfInstances;
+  }
+}
+
+export default Dwarf;
